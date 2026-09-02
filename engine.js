fb6132d3adca353f1d057ae2babd453675accdab
Abdulazeez Adeiza Abdulamlik <adeizaazeez@yahoo.com>
2026-09-02 10:19:32 +0100

    Add rounded WPM calculator

--- engine.js
+++ engine.js
@@ -12,3 +12,19 @@ console.log(correctKeystrokes);
 
 const accuracyPercentage = (correctKeystrokes, userKeystrokes) => (correctKeystrokes/(userKeystrokes.length)) * 100;
 console.log("The Accuracy Percentage is " + accuracyPercentage(correctKeystrokes, userKeystrokes) + "%");
+
+const userSession = {
+  username: "Bello",
+  token: undefined,
+  logout: () => console.log("Logging out...")
+};
+
+const flatPack = JSON.stringify(userSession);
+console.log(flatPack);
+
+
+const totalKeystrokes = 250;
+const timeInMinutes = 1.5;
+
+const calculateWPM = (totalKeystrokes, timeInMinutes) => (totalKeystrokes / 5) / timeInMinutes;
+console.log("The Words per Minutes is " + Math.round(calculateWPM(totalKeystrokes, timeInMinutes)));
